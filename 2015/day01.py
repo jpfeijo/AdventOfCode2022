8d062b2e75fdeea2bcd28cb7fefa6514a46a8629
jpfeijo <j.feijo@edu.pucrs.br>
2023-06-12 09:08:04 -0300

    day01 2 stars

--- 2015/day01.py
+++ 2015/day01.py
@@ -1,11 +1,18 @@
 floor = 0
 
+
 with open('2015/input.txt') as file:
     for line in file:
-      for character in line:
+      for index, character in enumerate(line):
+         if floor == -1:
+               print(index)
+               break
+         
          if character == "(":
             floor += 1
+
          elif character == ")":
             floor -= 1
+
           
-print(floor)
+# print(floor)
