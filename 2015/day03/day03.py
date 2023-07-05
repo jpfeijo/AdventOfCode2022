11ea78cb014a3e75656913aa996f70e6c76b84bc
jpfeijo <j.feijo@edu.pucrs.br>
2023-07-05 10:02:52 -0300

    day 03 - 2 stars

--- 2015/day03/day03.py
+++ 2015/day03/day03.py
@@ -1,25 +1,31 @@
 visited_houses = set()
-x = y = 0
+x = y = xr = yr = 0  
 
-visited_houses.add((x, y))
-
-print(visited_houses)
+visited_houses.add((x, y))  
 
 with open('2015/day03/input.txt') as file:
     for line in file:
-        for char in line:
-            if char == '>':
-              x += 1
-
-            if char == '<':
-              x -= 1
-
-            if char == '^':
-              y += 1
-
-            if char == 'v':
-                y -= 1
-
-            visited_houses.add((x, y))
+        for i, char in enumerate(line):
+            if i % 2 == 0:
+                if char == '>':
+                    x += 1
+                elif char == '<':
+                    x -= 1
+                elif char == '^':
+                    y += 1
+                elif char == 'v':
+                    y -= 1
+                visited_houses.add((x, y))
+            
+            else:  
+                if char == '>':
+                    xr += 1
+                elif char == '<':
+                    xr -= 1
+                elif char == '^':
+                    yr += 1
+                elif char == 'v':
+                    yr -= 1
+                visited_houses.add((xr, yr))
 
 print(len(visited_houses))
