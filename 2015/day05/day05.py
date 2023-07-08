d6c21986ff6b3ba1ec33b0e2599b20911be8d8bd
jpfeijo <j.feijo@edu.pucrs.br>
2023-07-07 23:21:11 -0300

    day 05 - 1 star

--- 2015/day05/day05.py
+++ 2015/day05/day05.py
@@ -3,3 +3,30 @@
 # At least one letter that appears twice in a row
 # Does not contain the strings ab, cd, pq, or xy
 # How many strings are nice?
+
+nice_strings = 0
+
+with open('2015/day05/input.txt') as file:
+    for line in file:
+        
+        vowels = 0
+        has_double_letter = False
+        has_prohibited_string = False
+        prev_letter = ''
+
+        for char in line:
+            if char in 'aeiou':
+                vowels += 1
+            
+            if char + prev_letter == char + char:
+                has_double_letter = True
+
+            if prev_letter + char in ['ab', 'cd', 'pq', 'xy']:
+                has_prohibited_string = True
+
+            prev_letter = char
+
+        if vowels >= 3 and has_double_letter and not has_prohibited_string:
+            nice_strings += 1
+
+print(nice_strings)
