6812f95a0d4662babb4ff1abc3d35cbf29925e79
jpfeijo <j.feijo@edu.pucrs.br>
2023-07-10 23:36:00 -0300

    day 02 - 2 stars

--- 2015/day05/day05.py
+++ 2015/day05/day05.py
@@ -1,32 +1,20 @@
-# Nice string 
-# At least 3 vowels
-# At least one letter that appears twice in a row
-# Does not contain the strings ab, cd, pq, or xy
-# How many strings are nice?
-
 nice_strings = 0
 
 with open('2015/day05/input.txt') as file:
     for line in file:
-        
-        vowels = 0
-        has_double_letter = False
-        has_prohibited_string = False
-        prev_letter = ''
+        has_repeat_pair = False
+        has_repeat_with_one_between = False
 
-        for char in line:
-            if char in 'aeiou':
-                vowels += 1
-            
-            if char + prev_letter == char + char:
-                has_double_letter = True
+        for i in range(len(line) - 2):
+            pair = line[i:i+2]
+            if pair in line[i+2:]:
+                has_repeat_pair = True
 
-            if prev_letter + char in ['ab', 'cd', 'pq', 'xy']:
-                has_prohibited_string = True
+        for i in range(len(line) - 2):
+            if line[i] == line[i+2]:
+                has_repeat_with_one_between = True
 
-            prev_letter = char
-
-        if vowels >= 3 and has_double_letter and not has_prohibited_string:
+        if has_repeat_pair and has_repeat_with_one_between:
             nice_strings += 1
 
 print(nice_strings)
