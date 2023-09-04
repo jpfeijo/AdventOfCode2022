c33ba4f7a3cf67cbfcadfd226a504cdcdc493c4e
jpfeijo <j.feijo@edu.pucrs.br>
2023-09-04 17:35:53 -0300

    day01 2016 2 stars

--- 2016/day01/day01.py
+++ 2016/day01/day01.py
@@ -2,21 +2,24 @@ with open('2016/day01/input.txt') as file:
     for line in file:
         instructions = line.split(', ')
 
+visited = set()
 x, y = 0, 0
 dx, dy = 0,  1
 
 for instruc in instructions:
-    turn, steps = str(instruc[0]), int(instruc[1:])
-
-    if turn == 'R':
+    turn, steps = instruc[0], int(instruc[1:])
+    if turn == "R":
         dx, dy = dy, -dx
     else:
         dx, dy = -dy, dx
 
-    x += dx * steps
-    y += dy * steps
+    for _ in range(steps):
+        x += dx
+        y += dy
 
+        if (x, y) in visited:
+            distance = abs(x) + abs(y)
+            print(distance)
+            break
 
-distance = abs(x) + abs(y)
-
-print(distance)
+        visited.add((x, y))
